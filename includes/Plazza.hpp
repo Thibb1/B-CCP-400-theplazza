199bc20ce56526acec364699d51fc906d78a0e84
Thibb1 <thibault.brumaire@gmail.com>
2022-04-21 11:37:37 +0200

    [+] init

---
+++ includes/Plazza.hpp
@@ -0,0 +1,9 @@
+/*
+** EPITECH PROJECT, 2022
+** Plazza.hpp
+** File description:
+** default includes
+*/
+
+#pragma once
+
---
+++ src/Main.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2022
+** Main.cpp
+** File description:
+** main
+*/
+
+#include "Plazza.hpp"
+
+int main(int ac, char **av)
+{
+    return 84;
+}
