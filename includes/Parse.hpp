042992dc806c0748160f7c8f2642c72e093e93e3
Marine Poteau <marinepoteau@MacBook-Pro-de-Marine.local>
2022-05-06 11:36:38 +0200

    [+] public

--- includes/Parse.hpp
+++ includes/Parse.hpp
@@ -12,12 +12,12 @@
 class Parse {
 
     public:
-        Parse(int NbArguments, char **Arguments);
-        virtual ~Parse() = default;
-        int CheckArguments(int NbArguments, char **Arguments);
-
-    private:
         float CookingTime;
         int Cooks;
         int RefillTime;
+
+    public:
+        Parse(int NbArguments, char **Arguments);
+        virtual ~Parse() = default;
+        int CheckArguments(int NbArguments, char **Arguments);
 };
