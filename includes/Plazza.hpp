179b4687b5dc2f3831f3646b3f8cfa4207bb9c58
Marine Poteau <marinepoteau@MacBook-Pro-de-Marine.local>
2022-05-06 12:08:18 +0200

    [+] modified plazza files

--- includes/Plazza.hpp
+++ includes/Plazza.hpp
@@ -16,5 +16,22 @@
 #include "PlazzaError.hpp"
 #include "RegexUtils.hpp"
 
+enum PizzaType
+{
+    Regina = 1,
+    Margarita = 2,
+    Americana = 4,
+    Fantasia = 8
+};
+
+enum PizzaSize
+{
+    S = 1,
+    M = 2,
+    L = 4,
+    XL = 8,
+    XXL = 16
+};
+
 #define MATCH "(?: *([[:alpha:]]+) (S|M|L|XL|XXL) x(\\d+))"
 #define EXIT_ERROR 84
--- src/plazza.cpp
+++ src/plazza.cpp
@@ -6,24 +6,9 @@
 */
 
 #include "Plazza.hpp"
+
 plazza::plazza(/* args */)
 {
-    std::thread first ();
-    std::thread second ();
-
-    while (1)
-    {
-        std::cin >> plazza::type;
-        if ("regina" == type)
-            std::cout << "ok" << std::endl;
-        else if ("americana" == s)
-            std::cout << "ok" << std::endl;
-        else {
-            std::cerr << "Error: not a pizza." << std::endl;
-            return (84);
-        }
-    }
-    return (0); 
 }
 
 plazza::~plazza()
